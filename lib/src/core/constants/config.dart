5a41396d214d56fa194e6a55d7fb1cff09b8c1f4
Nikita Mikhailov <n.mikhailov55@gmail.com>
2024-10-24 00:15:51 +0300

    add config classes

---
+++ lib/src/core/constants/config.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+
+/// [Config] Main information about the application configuration.
+class Config {
+  const Config();
+
+  /// environment from starter
+  Environment get environment => Environment.from(
+        const String.fromEnvironment(
+          'ENVIRONMENT',
+        ),
+      );
+}
+
+/// [Environment] describes in what mode the app was started.
+enum Environment {
+  dev,
+  prod;
+
+  static from(
+    String env,
+  ) =>
+      switch (env) {
+        'dev' => Environment.dev,
+        'prod' => Environment.prod,
+        _ => kReleaseMode ? Environment.prod : Environment.dev,
+      };
+}
